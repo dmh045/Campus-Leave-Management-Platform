f85aa35fa8bbde2efc4c47b483266834aa964485
dongc1c <dongc1q1@gmail.com>
2025-12-25 10:49:09 +0800

    Delete demo directory

--- demo/src/main/java/com/example/leavesystem/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.leavesystem.service;
-import com.example.leavesystem.dto.LoginResponse;
-import com.example.leavesystem.dto.LoginRequest;
-
-public interface AuthService {
-    LoginResponse login(LoginRequest request);
-    void logout(String token);;
-}
